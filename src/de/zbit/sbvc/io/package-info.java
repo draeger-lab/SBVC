070e070fc68c614f21d77420be98215efb59bd2e
Andreas Dräger <andreas.draeger@uni-tuebingen.de>
2012-08-24 15:19:03 +0000

    Added package-info.java files to each package to facilitate the documentation.

---
+++ src/de/zbit/sbvc/io/package-info.java
@@ -0,0 +1,29 @@
+/*
+ * $Id$
+ * $URL$
+ * ---------------------------------------------------------------------
+ * This file is part of SBVC, the systems biology visualizer and
+ * converter. This tools is able to read a plethora of systems biology
+ * file formats and convert them to an internal data structure.
+ * These files can then be visualized, either using a simple graph
+ * (KEGG-style) or using the SBGN-PD layout and rendering constraints.
+ * Some currently supported IO formats are SBML (+qual, +layout), KGML,
+ * BioPAX, SBGN, etc. Please visit the project homepage at
+ * <http://www.cogsys.cs.uni-tuebingen.de/software/SBVC> to obtain the
+ * latest version of SBVC.
+ *
+ * Copyright (C) 2012 by the University of Tuebingen, Germany.
+ *
+ * SBVC is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+
+/**
+ * 
+ */
+package de.zbit.sbvc.io;
